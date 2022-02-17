3290b7139f521026f6657ba8d90058770d1e5391
MThos <MThos@users.noreply.github.com>
2022-02-17 04:23:49 -0500

    Initial Commit

---
+++ Stock Average Down Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stock Average Down Calculator
+//
+//  Created by Mykel Agathos on 2022-02-17.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
